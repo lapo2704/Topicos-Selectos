5076173af7f8c4fe2b2e8450e882b3adeefbafeb
lapo2704 <rriveralaporte_04@hotmail.com>
2016-06-23 11:20:12 -0600

    WCF

--- Groupware.Calentamiento/Tests.Calculadora/TestDivision.cs
+++ Groupware.Calentamiento/Tests.Calculadora/TestDivision.cs
@@ -11,7 +11,7 @@ namespace Tests.Calculadora
     [TestClass]
     public class TestDivision
     {
-       [TestMethod]
+        /*[TestMethod]
         public void DivisionConErrores()
         {
             Numero elPrimerOperando = new Numero("78G", 16);
@@ -21,7 +21,7 @@ namespace Tests.Calculadora
 
             //Invoque el metodo que se prueba
             var laReferencia = new Core.Numero.Dominio.Acciones.Division();
-            elResultadoReal = laReferencia.OperarDivision(elPrimerOperando, elSegundoOperando,elResultadoEsperado);
+            elResultadoReal = laReferencia.OperarDivision(elPrimerOperando, elSegundoOperando, elResultadoEsperado);
 
             //Verificar si el resultado obtenido es el mismo que es el espereado
             Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
@@ -37,27 +37,33 @@ namespace Tests.Calculadora
 
             //Invoque el metodo que se prueba
             var laReferencia = new Core.Numero.Dominio.Acciones.Division();
-            elResultadoReal = laReferencia.OperarDivision(elPrimerOperando, elSegundoOperando,elResultadoEsperado);
+            elResultadoReal = laReferencia.OperarDivision(elPrimerOperando, elSegundoOperando, elResultadoEsperado);
 
             //Verificar si el resultado obtenido es el mismo que es el espereado
             Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
-        }
+        }*/
 
         [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
         public void DivisionConCero()
         {
-             Numero elPrimerOperando = new Numero("5", 10);
-                Numero elSegundoOperando = new Numero("0", 10);
-                Numero elResultadoEsperado = new Numero("∞", 10);
-                Numero elResultadoReal;
+            try { 
+            Numero elPrimerOperando = new Numero("5", 10);
+            Numero elSegundoOperando = new Numero("0", 10);
+            Numero elResultadoEsperado = new Numero("∞", 10);
+            Numero elResultadoReal;
 
-                //Invoque el metodo que se prueba
-                var laReferencia = new Core.Numero.Dominio.Acciones.Division();
-                elResultadoReal = laReferencia.OperarDivision(elPrimerOperando, elSegundoOperando,elResultadoEsperado);
+            //Invoque el metodo que se prueba
+            var laReferencia = new Core.Numero.Dominio.Acciones.Division();
+            elResultadoReal = laReferencia.OperarDivision(elPrimerOperando, elSegundoOperando, elResultadoEsperado);
 
-                //Verificar si el resultado obtenido es el mismo que es el espereado
-                Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
-            }           
+            //Verificar si el resultado obtenido es el mismo que es el espereado
+            Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
+        }        
+         catch (System.DivideByZeroException e) {
+               StringAssert.Contains(e.Message,e.ToString());
+            }
+        }   
         }
     }
 
